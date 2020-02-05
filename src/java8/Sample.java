e5cc0391b88b7313a7734e0097554c3578222b6a
SURYA <suryamss09@gmail.com>
2020-02-05 22:10:04 +0530

    CompletableFuture with Supply interface and supplyAsync method

--- src/java8/Sample.java
+++ src/java8/Sample.java
@@ -1,24 +1,27 @@
 package java8;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class Sample {
+	
+	public static CompletableFuture<Integer> create(Integer n) {
+		//Run a task specified by a Supplier Object
+				return CompletableFuture.supplyAsync(() -> n*2 );
+	}
 
 	public static void main(String[] args) {
-		//Run a task specified by a Runnable Object
-		CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> {
-			System.out.println("In Completable future");
-		});
+		create(19).thenApply(n -> n+1).thenAccept(System.out::println).thenRun(() -> System.out.println("Run method"));
 		
-		try {
-			completableFuture.get();
-		} catch (InterruptedException | ExecutionException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
 
 
-//OutPut --> In Completable future
+/*OutPut --> 39
+			Run method
+			
+			thenApply accepts and retuns a value
+			thenAccept accept and does not return any value.
+			thenRun does not accept and does not return any value.
+			
+*/
